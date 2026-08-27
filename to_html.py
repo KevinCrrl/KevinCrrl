45b6de26e14b2082fbced23941d51b8001efbd5c
KevinCrrl <y62257136@gmail.com>
2026-08-27 10:27:28 -0500

    Add markdown-based pkgbuild-parser translation, add license, remove legacy KPA warning

---
+++ to_html.py
@@ -0,0 +1,38 @@
+# Copyright (C) 2026 KevinCrrl
+# SPDX-License-Identifier: Apache-2.0
+
+import urllib.request
+
+import markdown
+
+
+def get_html(body: str, title: str = "KevinCrrl") -> str:
+    return f"""<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>{title}</title>
+    <link rel="stylesheet" href="/KevinCrrl/estilos.css">
+    <meta name="viewport" content="width=device-width,initial-scale=1"/>
+</head>
+
+<body>
+{body}
+<a href="/KevinCrrl/index.html">Back to homepage</a>
+</body>
+
+</html>
+"""
+
+
+urllib.request.urlretrieve(
+    "https://github.com/KevinCrrl/pkgbuild_parser/raw/refs/heads/main/README.md",
+    "pp_readme.md",
+)
+
+with (
+    open("documentation/pkgbuild_parser/index.html", "w", encoding="utf-8") as html,
+    open("pp_readme.md", "r", encoding="utf-8") as md,
+):
+    html.write(get_html(markdown.markdown(md.read(), extensions=["tables", "fenced_code"]), "Pkgbuild Parser"))
